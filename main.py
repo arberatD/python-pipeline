db417adb5fbe6343f2964b0efbc23346ad8f6588
arberatD <arberatdushi@gmail.com>
2024-01-31 12:46:02 +0100

    main changed

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ def get_full_name():
     first_name = input("Bitte geben Sie Ihren Vornamen ein: ")
     last_name = input("Bitte geben Sie Ihren Nachnamen ein: ")
     return first_name + " " + last_name
+
+# Rufen Sie die Funktion hier auf und drucken Sie das Ergebnis
+full_name = get_full_name()
+print(full_name)
